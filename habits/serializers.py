45e307dc0fc76a83a17dd63b700e5ec3491b5e2d
SergeyZaprudnov <szaprudnov@hotmail.com>
2023-09-13 18:48:13 +0300

    adding and entering a description of the serializer

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,14 +1,33 @@
-from django.contrib.auth.hashers import make_password
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
-from users.models import User
+from habits.models import Habit
 
 
-class UserSerializer(ModelSerializer):
-    """Значение переданное пользователем"""
-    def validate_password(self, value: str) -> str:
-        return make_password(value)
+class HabitSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+        if self.instance:
+            if self.instance.related_habit and (data.get('reward') or data.get('related_habit')):
+                raise serializers.ValidationError(
+                    'Вы не можете выбрать награду и связанную с ней привычку за одну привычку')
+            elif self.instance.is_pleasant and (data.get('reward') or data.get('related_habit')):
+                raise serializers.ValidationError(
+                    'Приятная привычка не может иметь награды или связанной с ней привычки')
+            elif data.get('is_pleasant') and (self.instance.reward or self.instance.related_habit):
+                raise serializers.ValidationError(
+                    'Приятная привычка не может иметь награды или связанной с ней привычки')
+            elif data.get('related_habit'):
+                if data.get('related_habit').is_pleasant is False:
+                    raise serializers.ValidationError('Соответствующая привычка должна быть приятной')
+            elif data.get('execution_time'):
+                if data.get('execution_time') > 120:
+                    raise serializers.ValidationError('Время выполнения вашей привычки должно быть не более 120 секунд')
+            elif data.get('frequency'):
+                if data.get('frequency') > 7:
+                    raise serializers.ValidationError('Выполнять привычку нельзя реже, чем 1 раз в 7 дней')
+
+        return data
 
     class Meta:
-        model = User
+        model = Habit
         fields = '__all__'
