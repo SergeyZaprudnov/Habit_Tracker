1710ed15f5c8cf84a18049105f3cc6fec3d63b85
SergeyZaprudnov <szaprudnov@hotmail.com>
2023-09-13 19:10:24 +0300

    adding and entering a description of views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    """Создание пользователя.
+       Для создания необходимо ввести логин, пароль и логин Telegram."""
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
